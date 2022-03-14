9d716b360d280f919a011010057fe3a25c401c91
senTristanC <wrecker331@gmail.com>
2022-03-13 21:08:22 -0700

    Added many new files. Figuring out how we will develop and build website without Angular or React.
    Begun routing and figuring out how Photoboards will be created

--- server.js
+++ server.js
@@ -5,15 +5,15 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const hostname = '192.168.0.14';
-const io = new Server(server);
+//const io = new Server(server);
 
 // Public is for development
-// build is for serving client neat code
+// build is for serving client code after it has been gulped or minified
 
-app.use(express.static('dev'));
+app.use(express.static('dev')); // Using Dev for serving files
 app.get('/', (req, res) => {
 
-    res.sendFile('index.html');
+    res.sendFile('index.html'); // Serves index.html
 
 });
 
