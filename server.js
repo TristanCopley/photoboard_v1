fe79d21ff3f62d752d423f00b753a9117d0cceac
senTristanC <wrecker331@gmail.com>
2022-03-17 20:20:27 -0700

    Removed Webpack package. Added more directories and slightly changed file structure. Finally, figured out routing! Next task is the main page.

--- server.js
+++ server.js
@@ -4,18 +4,28 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
+const path = require("path");
 const hostname =  'localhost';
 const io = new Server(server);
 
 // Public is for development
 // build is for serving client code after it has been gulped or minified
 
-app.use(express.static('dev/default')); // Using Dev for serving files
-app.get('/', (req, res) => {
 
-    res.sendFile('index.html'); // Serves index.html
+app.use(express.static('public/default')); // Using Dev for serving files
+
+app.get('/signup', (req, res) => {
+
+    res.sendFile(__dirname + '/public/default/signup.html'); // Serves index.html
 
 });
+
+app.get('/', (req, res) => {
+
+    res.sendFile(__dirname + '/public/default/index.html'); // Serves index.html
+
+});
+
 
 
 app.listen(port, () => {
