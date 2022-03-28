4023539d63c057843cc0b204c61c2a936b3d3b6d
Tristan Copley <89219797+TristanCopley@users.noreply.github.com>
2022-03-27 17:22:56 -0700

    Delete environment.js
    Accidently committed. That's my bad.

--- environment.js
+++
@@ -1,7 +0,0 @@
-module.exports = env = {
-
-    host: 'localhost',
-    privateKey: '7ef74cd7f998e060041a2d74e08caf7c608919062ffe76eca7543f5c3c7e00e7'
-
-}
-// Never commit this file as it contains information that will risk user and server security
